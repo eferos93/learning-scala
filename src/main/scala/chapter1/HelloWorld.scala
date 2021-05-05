035782f252aaa43f74d2b276bdf9cf30b7ae28bf
eferos93 <eros.fabrici@gmail.com>
2021-05-05 14:24:01 +0200

    added hello world

---
+++ src/main/scala/chapter1/HelloWorld.scala
@@ -0,0 +1,6 @@
+package learningscala
+package chapter1
+
+object HelloWorld extends App {
+  println("Hello World!")
+}
---
+++ src/main/scala/chapter1/HelloWorldMain.scala
@@ -0,0 +1,9 @@
+package learningscala
+package chapter1
+
+object HelloWorldMain {
+//  Unit type is similar to void in java
+  def main(args: Array[String]): Unit = {
+    println("Hello World from main")
+  }
+}
