7f521efedc915c4b5a5e7c12c61fd2606e4959ee
Eros Fabrici <eros.fabrici@gmail.com>
2021-06-01 16:39:52 +0200

    polymorfic functions

--- src/main/scala/chapter3/GenericTypedFunctions.scala
+++ src/main/scala/chapter3/GenericTypedFunctions.scala
@@ -22,4 +22,27 @@ object GenericTypedFunctions extends App {
   applyDiscount[String]("Coupon123")
   applyDiscount[Double](10)
 
+//  POLYMORPHIC TYPED FUNCTIONS
+  def applyDiscountWithReturnType[T](discount: T): Seq[T] = {
+    discount match {
+      case d: String =>
+        println(s"Lookup percentage discount in database for $d")
+        Seq[T](discount)
+      case d: Double =>
+        println(s"Apply discount $d")
+        Seq[T](discount)
+      case _ =>
+        println("Unsupported disocunt type")
+        Seq[T](discount)
+    }
+  }
+
+  println("\nStep 4: How to call a generic typed function which also has a generic return type")
+  println(s"Result of applyDiscountWithReturnType with String parameter = ${applyDiscountWithReturnType[String]("COUPON_123")}")
+
+  println()
+  println(s"Result of applyDiscountWithReturnType with Double parameter = ${applyDiscountWithReturnType[Double](10.5)}")
+
+  println()
+  println(s"Result of applyDiscountWithReturnType with Char parameter = ${applyDiscountWithReturnType[Char]('U')}")
 }
