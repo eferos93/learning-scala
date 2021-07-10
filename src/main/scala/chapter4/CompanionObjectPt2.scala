f2ef1c71c5ea8414412eefaeb8e0f51ebdf9594a
eferos93 <eros.fabrici@gmail.com>
2021-07-10 18:04:10 +0200

    CompanionObjects

--- src/main/scala/chapter4/CompanionObjectPt2.scala
+++ src/main/scala/chapter4/CompanionObjectPt2.scala
@@ -2,5 +2,20 @@ package learningscala
 package chapter4
 
 object CompanionObjectPt2 extends App {
+  class Donut(name: String, productCode: Option[Long] = None) {
+    def print(): Unit = println(s"Donut name = $name, productCode = $productCode")
+  }
 
+  class GlazedDonut(name: String) extends Donut(name)
+  class VanillaDonut(name: String) extends Donut(name)
+
+  object Donut {
+    def apply(name: String): Donut = {
+      name match {
+        case "Glazed Donut" => new GlazedDonut(name)
+        case "Vanilla Donut" => new VanillaDonut(name)
+        case _ => new Donut(name)
+      }
+    }
+  }
 }
